a4ea2ac2806a8048186855cd1f1f61278f139b35
Phil Hunt <phil@deviantlogic.co.uk>
2020-03-20 18:29:06 +0000

    4 Clojure problems

--- src/learnclojure.clj
+++ src/learnclojure.clj
@@ -1,6 +1,7 @@
 (ns learnclojure)
 
-(require '[clojure.repl :refer [doc]])
+(require '[clojure.repl :refer [doc]]
+         )
 
 (print "Hello World")
 
@@ -32,11 +33,23 @@
 (doc hello)
 
 (defn my-set [x y]
+  "JoC2 book version won't make a set using e.g. a a, this work around this"
   (println (str "Make me a set bitch. Use " x " and " y))
   (set (vector x y)))
 
 (my-set 'a 'bq)
 
-(def somens [1 2 3 3 4 5])
+;; 4Clojure #29 - return seq of upper case chars only
 
-(apply + somens)
+(= (#(clojure.string/join (re-seq #"[A-Z]" %)) "HeLlO, WoRlD!") "HLOWRD")
+
+;; 4Clojure #30 - remove consecutive dupliates and return
+
+(= (apply str ( #(dedupe (seq %)) "Leeeeeerrroyyy")) "Leroy")
+
+(= ( #(map first (partition-by identity %)) [1 1 2 3 3 2 2 3]) '(1 2 3 2 3))
+
+;; 4Clojure #39 - interleave
+
+(= (#(map first [%1 %2]) [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c))
+; I think I need to understand 'recur' and/or 'loop' here, vs CAR/CDR recursion
